50ee04a9cb54496e30af30667b22ea92d75af451
magamex <magamex.22@gmail.com>
2022-02-18 23:29:06 -0300

    creacion de proyecto y agregar primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  nombre: string = 'matias';
+
+  mostrarNombre(){
+    console.log(this.nombre);
+  }
 }

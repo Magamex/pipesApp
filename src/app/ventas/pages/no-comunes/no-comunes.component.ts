3c9d2286846adb580dc0538bd69764930f4e2250
magamex <magamex.22@gmail.com>
2022-02-24 00:01:44 -0300

    pipes no comunes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,6 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoComunesComponent implements OnInit {
 
+  // I18nSelectPipe
+  nombre: string = 'Matias';
+  genero: string = 'masculino';
+
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla',
+  }
+
+  // I18nPluralPipe
+  clientes: string[] = ['Maria','Pedro','Cata'];
+
+  clientesMapa = {
+    '=0': 'No hay clientes esperando',
+    '=1': 'Tenemos 1 cliente esperando',
+    '=2': 'Tenemos 2 clientes esperando',
+    'other': 'Tenemos # clientes esperando'
+
+  }
+
   constructor() { }
 
   ngOnInit(): void {

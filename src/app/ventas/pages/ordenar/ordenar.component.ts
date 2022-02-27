737a5dd9f6394bbc61559da8d6cc219dd935518e
magamex <magamex.22@gmail.com>
2022-02-27 19:56:15 -0300

    Pipes personalizados

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -8,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdenarComponent implements OnInit {
 
+  test:string = 'Hola Mundo';
+  enMayusculas: boolean = true;
+
+  heroes:Heroe[] = [
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Flash',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    }
+  ]
+
+  ordenarPor:string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarMayusculas(){
+    this.enMayusculas = !this.enMayusculas;
+  }
+
+  cambiarOrden(orden:string){
+    this.ordenarPor = orden;
   }
 
 }
